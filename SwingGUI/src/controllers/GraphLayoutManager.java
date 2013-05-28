95a77a00e991537c719e45a7af2f0d6cf896a53b
dvinokurov <dvinokurov777@gmail.com>
2013-05-28 23:29:24 +0300

    Graph layout manager finished

--- SwingGUI/src/controllers/GraphLayoutManager.java
+++ SwingGUI/src/controllers/GraphLayoutManager.java
@@ -37,6 +37,12 @@ public class GraphLayoutManager {
         return maxLength(graph.checkpointLines());
     }
 
+    public synchronized GraphLayoutElement[] getLayoutElements() {
+        GraphLayoutElement[] elements = layoutElementsFromVertexes();
+        setElementsPositions(elements);
+        return elements;
+    }
+
     private int maxLength(List<Graph.CheckpointLine> outerLines) {
         int max = -1;
         List<Graph.CheckpointLine> lines = outerLines;
@@ -44,55 +50,67 @@ public class GraphLayoutManager {
             if (lines.head().checkpoints().length() > max) max = lines.head().checkpoints().length();
             lines = lines.tail();
         }
+        if (max==-1)throw new IllegalStateException("Graph does not contain checkpoint lines or their sizes are wrong");
         return max;
     }
 
-    public GraphLayoutElement[] getLayoutElements(){
-        GraphLayoutElement[] elements = layoutElementsFromVertexes();
+    private GraphLayoutElement[] layoutElementsFromVertexes() {
+        GraphLayoutElement[] elements = new GraphLayoutElement[totalElements()];
+        List<Vertex> vertexesLeft = graph.vertexes();
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = new GraphLayoutElement(vertexesLeft.head());
+            vertexesLeft = vertexesLeft.tail();
+        }
+        return elements;
+    }
 
-        int maxElementsInLine=maxLength(graph.checkpointLines());
+    private void setElementsPositions(GraphLayoutElement[] elements) {
+        int maxElementsInLine = maxLength(graph.checkpointLines());
         int elementsInLine;
         int indexInLine;
+        int lineIndex;
         Graph.CheckpointLine currentLine;
         Vertex vertex;
-        for (int i = 0; i < elements.length; i++) {
-            vertex=elements[i].vertex;
-            currentLine=lineContainsVertex(vertex);
-            elementsInLine=currentLine.checkpoints().length();
-            indexInLine=indexInLine(vertex,currentLine);
+        for (GraphLayoutElement element : elements) {
+            vertex = element.vertex;
+            currentLine = lineContainsVertex(vertex);
+            elementsInLine = currentLine.checkpoints().length();
+            indexInLine = indexInLine(vertex, currentLine);
+            lineIndex = lineIndex(currentLine);
+            element.setX(lineIndex);
+            element.setY(maxElementsInLine * indexInLine / elementsInLine);
         }
-
-        return elements;
     }
 
-    private GraphLayoutElement[] layoutElementsFromVertexes() {
-        GraphLayoutElement[] elements=new GraphLayoutElement[totalElements()];
-        List<Vertex> vertexesLeft=graph.vertexes();
-        for (int i = 0; i < elements.length; i++) {
-            elements[i]=new GraphLayoutElement(vertexesLeft.head());
-            vertexesLeft=vertexesLeft.tail();
-        }
-        return elements;
-    }
-
-    private Graph.CheckpointLine lineContainsVertex(Vertex vertex){
-        List<Graph.CheckpointLine> lines=graph.checkpointLines();
-        while(!lines.isEmpty()){
-            if(lines.head().checkpoints().contains(vertex))return lines.head();
-            else lines=lines.tail();
+    private Graph.CheckpointLine lineContainsVertex(Vertex vertex) {
+        List<Graph.CheckpointLine> lines = graph.checkpointLines();
+        while (!lines.isEmpty()) {
+            if (lines.head().checkpoints().contains(vertex)) return lines.head();
+            else lines = lines.tail();
         }
         return null;
     }
 
-    private int indexInLine(Vertex vertex,Graph.CheckpointLine line){
-        int result=0;
-        List<Vertex> checkpointsLeft=line.checkpoints();
-        while(!checkpointsLeft.isEmpty()){
-            if(vertex.equals(checkpointsLeft.head()))return result;
+    private int indexInLine(Vertex vertex, Graph.CheckpointLine line) {
+        int result = 0;
+        List<Vertex> checkpointsLeft = line.checkpoints();
+        while (!checkpointsLeft.isEmpty()) {
+            if (vertex.equals(checkpointsLeft.head())) return result;
             result++;
-            checkpointsLeft=checkpointsLeft.tail();
+            checkpointsLeft = checkpointsLeft.tail();
         }
         throw new IllegalStateException("Current checkpoint line does not contain the vertex");
+    }
+
+    private int lineIndex(Graph.CheckpointLine line) {
+        int result = 0;
+        List<Graph.CheckpointLine> linesLeft = graph.checkpointLines();
+        while (!linesLeft.isEmpty()) {
+            if (line.equals(linesLeft.head())) return result;
+            result++;
+            linesLeft = linesLeft.tail();
+        }
+        throw new IllegalStateException("Graph does not contain the checkpoint line");
     }
 
 
